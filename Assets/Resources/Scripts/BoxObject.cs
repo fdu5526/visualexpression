2097023073934ee5346883529de208961d2586e9
Dave Yan <fdu5526@gmail.com>
2016-09-01 21:04:05 -0700

    sound and stuff

---
+++ Assets/Resources/Scripts/BoxObject.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoxObject : Physics2DBody {
+
+	bool isDragged;
+	float initZ;
+
+	// Use this for initialization
+	protected override void Awake () {
+		base.Awake();
+		initZ = transform.position.z;
+	}
+
+	void OnMouseDown () {
+		isDragged = true;
+	}
+
+	void OnMouseUp() {
+		isDragged = false;
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	if (isDragged) {
+			Vector3 m = Input.mousePosition;
+			m.z = 1f;
+			Vector2 mousePos = (Vector2)(Camera.main.ScreenToWorldPoint(m));
+			Vector3 newPos = new Vector3(mousePos.x, mousePos.y, initZ);
+			transform.position = newPos;
+	}
+		
+	}
+}
---
+++ Assets/Resources/Scripts/Physics2DBody.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody2D))]
+[RequireComponent (typeof (Collider2D))]
+public class Physics2DBody : MonoBehaviour {
+
+	protected Rigidbody2D rigidbody2d;
+	protected Collider2D collider2d;
+
+	// Use this for initialization
+	protected virtual void Awake () {
+		rigidbody2d = GetComponent<Rigidbody2D>();
+		collider2d = GetComponent<Collider2D>();
+	}
+}
