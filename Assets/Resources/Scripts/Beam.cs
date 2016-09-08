2fefae0c0a4605610ba69f6a42624aeeac2d52d1
Dave Yan <fdu5526@gmail.com>
2016-09-08 00:35:14 -0700

    added ambiguous

---
+++ Assets/Resources/Scripts/Beam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beam : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		transform.localScale = new Vector3(transform.localScale.x, transform.localScale.y * RandomS, transform.localScale.z * RandomS);
+	}
+
+	float RandomS { get { return Random.Range(0.1f, 2f); } }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Resources/Scripts/BeamSpawner.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeamSpawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Random.seed = 5;
+		/*for (int i = 0; i < 100; i++) {
+			GameObject g = (GameObject)Instantiate(Resources.Load("Prefabs/Beam"));
+			g.transform.position = new Vector3(RandomPosition, RandomPosition, RandomPosition);
+		}*/
+	}
+
+	float RandomPosition { get { return Random.Range(-10f, 100f); } }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
