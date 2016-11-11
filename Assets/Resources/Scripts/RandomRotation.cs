c2be7a9e7961ace09cc7db72f8b6464ce8e76e30
Dave Yan <fdu5526@gmail.com>
2016-11-10 21:08:44 -0800

    open and closed space done

---
+++ Assets/Resources/Scripts/RandomRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//float y = Random.Range(0f, 360f);
+		//transform.eulerAngles = new Vector3(0f, y, 0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
